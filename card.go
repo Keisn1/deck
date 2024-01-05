0bebae29422cdca58b3478499fdb49270a658cd9
Kay Freyer <kay.freyer@icloud.com>
2024-01-05 14:41:18 +0100

    Refactor to Opts

--- card.go
+++ card.go
@@ -58,7 +58,91 @@ func (c Card) String() string {
 	return fmt.Sprintf("%s of %ss", c.Rank.String(), c.Suit.String())
 }
 
-func defaultCards() (cards []Card) {
+type Opts struct {
+	rankFuncForSort func(c Card) int
+	Reverse         bool
+	NbrOfJokers     int
+	NbrOfDecks      int
+	SuitsToFilter   []Suit
+	RanksToFilter   []Rank
+	WithShuffle     bool
+}
+
+func getDefaultOpts() Opts {
+	return Opts{
+		rankFuncForSort: AbsRank,
+		Reverse:         false,
+		NbrOfJokers:     0,
+		NbrOfDecks:      1,
+		WithShuffle:     false,
+		SuitsToFilter:   []Suit{},
+		RanksToFilter:   []Rank{},
+	}
+}
+
+type OptFunc func(*Opts)
+
+func New(optFuncs ...OptFunc) []Card {
+	cards := getDefaultCards()
+	opts := getDefaultOpts()
+
+	for _, optFunc := range optFuncs {
+		optFunc(&opts)
+	}
+
+	cards = applyOpts(cards, opts)
+	return cards
+}
+
+func WithSort(rankFuncForSort func(c Card) int) OptFunc {
+	return func(o *Opts) {
+		o.rankFuncForSort = rankFuncForSort
+	}
+}
+
+func WithReverse() OptFunc {
+	return func(o *Opts) {
+		o.Reverse = true
+	}
+}
+
+func WithJokers(nbrOfJokers int) OptFunc {
+	return func(o *Opts) {
+		o.NbrOfJokers = nbrOfJokers
+	}
+}
+
+func WithNbrOfDecks(nbrOfDecks int) OptFunc {
+	return func(o *Opts) {
+		o.NbrOfDecks = nbrOfDecks
+	}
+}
+
+func WithFilterSuits(suitsToFilter []Suit) OptFunc {
+	return func(o *Opts) {
+		o.SuitsToFilter = suitsToFilter
+	}
+}
+
+func WithFilterRanks(ranksToFilter []Rank) OptFunc {
+	return func(o *Opts) {
+		o.RanksToFilter = ranksToFilter
+	}
+}
+
+func WithShuffle(withShuffle bool) OptFunc {
+	return func(o *Opts) {
+		o.WithShuffle = withShuffle
+	}
+}
+
+func WithMultipleDecks(nbrOfDecks int) OptFunc {
+	return func(o *Opts) {
+		o.NbrOfDecks = nbrOfDecks
+	}
+}
+
+func getDefaultCards() (cards []Card) {
 	for _, rank := range ranks {
 		for _, suit := range suits {
 			cards = append(cards, Card{
@@ -70,104 +154,47 @@ func defaultCards() (cards []Card) {
 	return
 }
 
-func New(optFuncs ...func([]Card) []Card) []Card {
-	cards := defaultCards()
-	defaultOps := DefaultOpts()
+func applyOpts(cards []Card, opts Opts) []Card {
+	cards = applyJokers(opts.NbrOfJokers)(cards)
+	cards = applyMultipleDecks(opts.NbrOfDecks)(cards)
 
-	// for _, opt := range opts {
-	// 	cards = opt(cards)
-	// }
-
-	cards = Jokers(defaultOps.NbrOfJokers)(cards)
-	cards = MultipleDecks(defaultOps.NbrOfDecks)(cards)
-	if defaultOps.Shuffle {
-		cards = Shuffle(cards)
+	if opts.WithShuffle {
+		cards = applyShuffle(cards)
 	}
-	for _, rank := range defaultOps.FilterRanks {
-		cards = Filter(
+
+	for _, rank := range opts.RanksToFilter {
+		cards = applyFilter(
 			func(c Card) bool { return c.Rank == rank },
 		)(cards)
 	}
 
-	for _, suit := range defaultOps.FilterSuits {
-		cards = Filter(
+	for _, suit := range opts.SuitsToFilter {
+		cards = applyFilter(
 			func(c Card) bool { return c.Suit == suit },
 		)(cards)
 	}
 
-	less := func(cards []Card) func(i, j int) bool {
+	comp := getCompFunc(opts)
+	sort.Slice(cards, comp(cards))
+	return cards
+}
+
+func getCompFunc(opts Opts) func(cards []Card) func(i, j int) bool {
+	return func(cards []Card) func(i, j int) bool {
 		return func(i, j int) bool {
-			return defaultOps.Sort(cards[i]) < defaultOps.Sort(cards[j])
+			if opts.Reverse {
+				return opts.rankFuncForSort(cards[i]) > opts.rankFuncForSort(cards[j])
+			}
+			return opts.rankFuncForSort(cards[i]) < opts.rankFuncForSort(cards[j])
 		}
 	}
-
-	cards = Sort(less)(cards)
-
-	return cards
 }
 
-func applyOpts(cards []Card) []Card {
-
-}
-
-type Opts struct {
-	NbrOfJokers int
-	NbrOfDecks  int
-	Shuffle     bool
-	FilterSuits []Suit
-	FilterRanks []Rank
-	Sort        func(c Card) int
-}
-
-func DefaultOpts() Opts {
-	return Opts{
-		NbrOfJokers: 0,
-		NbrOfDecks:  1,
-		Shuffle:     false,
-		FilterSuits: []Suit{},
-		FilterRanks: []Rank{},
-		Sort:        absRank,
-	}
-}
-
-func DefaultSort(cards []Card) []Card {
-	sort.Slice(cards, Less(cards))
-	return cards
-}
-
-func Less(cards []Card) func(i, j int) bool {
-	return func(i, j int) bool {
-		return absRank(cards[i]) < absRank(cards[j])
-	}
-}
-
-func Sort(less func(cards []Card) func(i, j int) bool) func(cards []Card) []Card {
-	return func(cards []Card) []Card {
-		sort.Slice(cards, less(cards))
-		return cards
-	}
-}
-
-func absRank(c Card) int {
+func AbsRank(c Card) int {
 	return int(c.Suit)*int(maxRank) + int(c.Rank)
 }
 
-func Shuffle(cards []Card) []Card {
-	rand.Shuffle(len(cards), func(i, j int) { cards[i], cards[j] = cards[j], cards[i] })
-	return cards
-}
-
-// func Shuffle1(cards []Card) []Card {
-// 	ret := make([]Card, len(cards))
-// 	r := rand.New(rand.NewSource(time.Now().Unix()))
-// 	perm := r.Perm(len(cards))
-// 	for i, j := range perm {
-// 		ret[i] = cards[j]
-// 	}
-// 	return ret
-// }
-
-func Jokers(nbr int) func(cards []Card) []Card {
+func applyJokers(nbr int) func(cards []Card) []Card {
 	return func(cards []Card) []Card {
 		for i := 0; i < nbr; i++ {
 			cards = append(cards, Card{Suit: Joker, Rank: Rank(i)})
@@ -176,16 +203,17 @@ func Jokers(nbr int) func(cards []Card) []Card {
 	}
 }
 
-func checkRankInRanks(givenRank Rank, ranks []Rank) (rankInRanks bool) {
-	for _, rank := range ranks {
-		if givenRank == rank {
-			return true
+func applyMultipleDecks(nbrOfDecks int) func([]Card) []Card {
+	return func(cards []Card) []Card {
+		var newDeck []Card
+		for i := 0; i < nbrOfDecks; i++ {
+			newDeck = append(newDeck, cards...)
 		}
+		return newDeck
 	}
-	return
 }
 
-func Filter(filterFunc func(Card) bool) func([]Card) []Card {
+func applyFilter(filterFunc func(Card) bool) func([]Card) []Card {
 	return func(cards []Card) []Card {
 		var newCards []Card
 		for _, c := range cards {
@@ -197,12 +225,7 @@ func Filter(filterFunc func(Card) bool) func([]Card) []Card {
 	}
 }
 
-func MultipleDecks(nbrOfDecks int) func([]Card) []Card {
-	return func(cards []Card) []Card {
-		var newDeck []Card
-		for i := 0; i < nbrOfDecks; i++ {
-			newDeck = append(newDeck, cards...)
-		}
-		return newDeck
-	}
+func applyShuffle(cards []Card) []Card {
+	rand.Shuffle(len(cards), func(i, j int) { cards[i], cards[j] = cards[j], cards[i] })
+	return cards
 }
--- card_test.go
+++ card_test.go
@@ -17,29 +17,29 @@ func TestLen(t *testing.T) {
 	want := 52
 	got := len(New())
 	if got != want {
-		t.Errorf("len(New1()) = \"%v\"; want \"%v\"", got, want)
+		t.Errorf("len(New()) = \"%v\"; want \"%v\"", got, want)
 	}
 }
 
 func TestDefaultSort(t *testing.T) {
 	want := Card{Rank: Ace, Suit: Diamond}
-	got := New(DefaultSort)[0]
+	got := New(WithSort(AbsRank))[0]
 	if got != want {
-		t.Errorf("New1(DefaultSort)[0] = \"%v\"; want \"%v\"", got.String(), want.String())
+		t.Errorf("New(WithSort(absRank))[0] = \"%v\"; want \"%v\"", got.String(), want.String())
 	}
 }
 
-func TestSortLess(t *testing.T) {
-	want := Card{Rank: Ace, Suit: Diamond}
-	got := New(Sort(Less))[0]
+func TestReverseSort(t *testing.T) {
+	want := Card{Rank: King, Suit: Club}
+	got := New(WithReverse())[0]
 	if got != want {
-		t.Errorf("New1(Sort(Less))[0] = \"%v\"; want \"%v\"", got.String(), want.String())
+		t.Errorf("New(WithReverse())[0] = \"%v\"; want \"%v\"", got.String(), want.String())
 	}
 }
 
 func TestNbrOfJokers(t *testing.T) {
 	want := 10
-	deck := New(Jokers(10))
+	deck := New(WithJokers(10))
 	count := 0
 	for _, c := range deck {
 		if c.Suit == Joker {
@@ -51,8 +51,8 @@ func TestNbrOfJokers(t *testing.T) {
 	}
 }
 
-func TestFilter(t *testing.T) {
-	deck := New(Filter(func(c Card) bool { return c.Rank == Two || c.Rank == Five }))
+func TestFilterRank(t *testing.T) {
+	deck := New(WithFilterRanks([]Rank{Two, Five}))
 	for _, c := range deck {
 		if c.Rank == Two || c.Rank == Five {
 			t.Errorf("Found %s in Deck, don't want %v", c.String(), []Rank{Two, Five})
@@ -60,9 +60,18 @@ func TestFilter(t *testing.T) {
 	}
 }
 
+func TestFilterSuit(t *testing.T) {
+	deck := New(WithFilterSuits([]Suit{Diamond}))
+	for _, c := range deck {
+		if c.Suit == Diamond {
+			t.Errorf("Found %s in Deck, don't want %v", c.String(), []Suit{Diamond})
+		}
+	}
+}
+
 func TestMultipleDecks(t *testing.T) {
 	want := 4 * 52
-	got := len(New(MultipleDecks(4)))
+	got := len(New(WithNbrOfDecks(4)))
 	if got != want {
 		t.Errorf("len(New1(MultipleDecks(4))) = \"%v\"; want \"%v\"", got, want)
 	}
